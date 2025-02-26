39bc59deed0728aefd86b233d9ff940e7c739b35
masterkiga <paul@neuralmimicry.ai>
2025-02-26 22:19:28 +0000

    Modify audiomanager

--- cmake-build-debug/_deps/json-subbuild/json-populate-prefix/tmp/json-populate-mkdirs.cmake
+++ cmake-build-debug/_deps/json-subbuild/json-populate-prefix/tmp/json-populate-mkdirs.cmake
@@ -3,8 +3,13 @@
 
 cmake_minimum_required(VERSION 3.5)
 
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "/home/pbisaacs/Developer/aarnn/cmake-build-debug/_deps/json-src")
+  file(MAKE_DIRECTORY "/home/pbisaacs/Developer/aarnn/cmake-build-debug/_deps/json-src")
+endif()
 file(MAKE_DIRECTORY
-  "/home/pbisaacs/Developer/aarnn/cmake-build-debug/_deps/json-src"
   "/home/pbisaacs/Developer/aarnn/cmake-build-debug/_deps/json-build"
   "/home/pbisaacs/Developer/aarnn/cmake-build-debug/_deps/json-subbuild/json-populate-prefix"
   "/home/pbisaacs/Developer/aarnn/cmake-build-debug/_deps/json-subbuild/json-populate-prefix/tmp"
